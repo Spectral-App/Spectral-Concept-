4324855f40658f375981c09c6950ec05949b5dab
stenaviv <99104850+stenaviv@users.noreply.github.com>
2024-07-15 23:39:28 -0500

    added an splash screen, inspired by the discord splash screen, looks cool

--- src/index.js
+++ src/index.js
@@ -7,9 +7,35 @@ if (require('electron-squirrel-startup')) {
 }
 
 let mainWindow;
+let splashWindow;
+const SPLASH_SCREEN_DELAY = 1000; // 1s delay, just for everyone to see our beautiful logo
 
-const createWindow = () => {
-  // Create the browser window.
+const createSplashScreen = () => {
+  splashWindow = new BrowserWindow({
+    width: 240,
+    height: 336,
+    frame: false,
+    transparent: true,
+    show: false,
+    resizable: false,
+  });
+
+  splashWindow.loadFile(path.join(__dirname, 'splash.html'));
+
+  splashWindow.once('ready-to-show', () => {
+    splashWindow.show();
+    setTimeout(() => {
+      createMainWindow();
+      splashWindow.close();
+    }, SPLASH_SCREEN_DELAY);
+  });
+
+  splashWindow.on('closed', () => {
+    splashWindow = null;
+  });
+};
+
+const createMainWindow = () => {
   mainWindow = new BrowserWindow({
     title: "Spectral",
     icon: "icon.ico",
@@ -21,7 +47,7 @@ const createWindow = () => {
     transparent: true,
     backgroundColor: '#00FFFFFF',
     autoHideMenuBar: true,
-    nodeIntegration: true,
+    show: false, // No mostrar la ventana principal inmediatamente
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
@@ -29,31 +55,27 @@ const createWindow = () => {
     },
   });
 
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow();
+  createSplashScreen();
 
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+      createMainWindow();
     }
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -81,6 +103,3 @@ ipcMain.on('close-window', () => {
 ipcMain.handle('is-maximized', () => {
   return mainWindow.isMaximized();
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
