e5ddfb3b3be0af741d02c9fb3efeaff1efb8f67c
Raulito2504 <raulariasrafael@gmail.com>
2024-07-24 07:33:46 -0500

    Cambios TOPOYIYIO

--- src/scripts/pageLoad/account/login.js
+++ src/scripts/pageLoad/account/login.js
@@ -1,4 +1,5 @@
-const loginForm = document.querySelector('#loginForm')
+function Registro() {
+    const loginForm = document.querySelector('#loginForm')
 loginForm.addEventListener('submit', (e)=>{
     e.preventDefault()
     const email = document.querySelector('#email').value
@@ -6,10 +7,13 @@ loginForm.addEventListener('submit', (e)=>{
     const Users = JSON.parse(localStorage.getItem('users')) || []
     const validUser = Users.find(user => user.email === email && user.password === password)
     if(!validUser){
-        return alert('Usuario y/o contraseña incorrectos!')
+        return alert('Correo y/o contraseña incorrectos!')
     }
     alert(`Bienvenido ${validUser.name}`)
     localStorage.setItem('login_success', JSON.stringify(validUser))
     window.location.href = 'index.html'   
 
 })
+}
+
+Registro()
